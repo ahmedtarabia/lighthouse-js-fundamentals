be52621ef3a3878a12ab67a98814308fc65e44a0
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-01-08 23:32:38 -0500

    modified a bit

--- hello.js
+++ hello.js
@@ -5,3 +5,16 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//part 2
+const sayHelloToConsole = function (name) {
+  console.log("Hello, "+name);
+}
+sayHelloToConsole('Peter');
+
+const returnSayHello = function(name) {
+  return "Hello, " +name;
+}
+const greeting = returnSayHello('Ahmed');
+// it will not print here but to make it print, do the following:
+console.log(greeting);
